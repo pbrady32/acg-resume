6ca7e10ed81744ddbe7a77a07f11a69768b8310f
Patrick Brady <pgbrady54@gmail.com>
2021-09-23 14:22:41 -0400

    Adding REST API JS code

--- content/static/site.js
+++ content/static/site.js
@@ -1,1 +1,7 @@
 // Call our API to track website hits
+const userAction = async () => {
+    const response = await fetch('http://api.pgbcloud.com/add');
+    const myJson = await response.json(); //extract JSON from the http response
+    // do something with myJson
+    console.log(myJson);
+}
