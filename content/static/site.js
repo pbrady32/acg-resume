ed8d2019b41b40c1e8c853ba704d362ed90792bf
Patrick Brady <pgbrady54@gmail.com>
2021-09-23 16:43:06 -0400

    Added code to increment visit count by 1 and return result to console

--- content/static/site.js
+++ content/static/site.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
     $.ajax({
         url: "https://api.pgbcloud.com/add"
-    }).then(function (data) {
-        console.log(data.result);
+    }).then(function (add_result) {
+        console.log(add_result.result);
     });
 
     $.ajax({
         url: "https://api.pgbcloud.com/count"
-    }).then(function (data) {
-        $('.views-count').html(data.count);
-        console.log(data.count)
+    }).then(function (count_data) {
+        $('.views-count').html(count_data.count);
+        console.log(count_data.count)
     });
 });
