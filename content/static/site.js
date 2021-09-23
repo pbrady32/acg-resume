9fb19950ba53da388c7e91e143288638ab07d26b
Patrick Brady <pgbrady54@gmail.com>
2021-09-23 18:10:41 -0400

    Modified code to try to get add_one() and get_count() rsults on same page. They were off by one

--- content/static/site.js
+++ content/static/site.js
@@ -1,14 +1,17 @@
-$(document).ready(function () {
-    $.ajax({
-        url: "https://api.pgbcloud.com/add"
-    }).then(function (add_result) {
-        console.log(add_result.result);
-    });
-
+function get_count() {
     $.ajax({
         url: "https://api.pgbcloud.com/count"
     }).then(function (count_data) {
         $('.views-count').html(count_data.count);
         console.log(count_data.count)
+    });
+}
+
+$(document).ready(function () {
+    $.ajax({
+        url: "https://api.pgbcloud.com/add"
+    }).then(function (add_result) {
+        console.log(add_result.result);
+        get_count();
     });
 });
